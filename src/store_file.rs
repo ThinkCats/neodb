b1a4430d4b5d64a9415917cd6dcd7889463a7aa9
wanglei <thinkcat@icloud.com>
2022-01-24 18:08:25 +0800

    add skiplist

--- src/store_file.rs
+++ src/store_file.rs
@@ -1,11 +1,36 @@
+use std::mem::size_of_val;
+
+#[derive(Debug, Clone)]
 pub struct SSDataEntry {
-    key: String,
-    value: String,
-    length: u64,
+    pub key: i64,
+    pub value: String,
+}
+
+impl PartialOrd for SSDataEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.key.cmp(&other.key))
+    }
+}
+
+impl PartialEq for SSDataEntry {
+    fn eq(&self, other: &Self) -> bool {
+        self.key == other.key && self.value == other.value
+    }
+}
+
+impl SSDataEntry {
+    pub fn length(&self) -> usize {
+        size_of_val(&self.key) + self.value.len()
+    }
 }
 
 pub struct SSFilter {
     //TODO bloom filter
+}
+
+pub struct SSBasic {
+    free: u64,
+    last_offset: u64,
 }
 
 pub struct SSTable {
--- tests/store_test.rs
+++ tests/store_test.rs
@@ -1,4 +1,6 @@
 use ndb::store::check_or_create_file;
+use ndb::store_file::SSDataEntry;
+use subway::skiplist::SkipList;
 
 #[test]
 fn test_check_file() {
@@ -16,4 +18,32 @@ fn test_create_file() {
     let result = check_or_create_file(path, size);
     println!("result is:{:?}", result);
     assert_eq!(true, result.is_ok());
+}
+
+#[test]
+fn test_skip_list() {
+    let mut list = SkipList::new();
+    let entry1 = SSDataEntry {
+        key: 1,
+        value: String::from("hello"),
+    };
+    println!("data entry size:{}", entry1.length());
+    list.insert(1, entry1);
+
+    let entry3 = SSDataEntry {
+        key: 3,
+        value: String::from("Ssss"),
+    };
+    list.insert(3, entry3);
+
+    let entry2 = SSDataEntry {
+        key: 2,
+        value: String::from("world"),
+    };
+    list.insert(2, entry2);
+    let values = list.collect();
+    println!("result:{:?}", values);
+
+    let dddd = list.get(&2);
+    println!("second ele:{:?}", dddd);
 }
